a879b5d52db4956e7b6b4ba69d3345691c5c5331
Will Rodman <willc.rodman@gmail.com>
2021-08-16 22:12:18 -0400

    working now

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from os import path
 from setuptools import setup
 
-this_directory = path.abspath(os.path.dirname(__file__))
+this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
